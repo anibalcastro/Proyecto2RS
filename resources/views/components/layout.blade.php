ad6b1b9ca1aed67876d1c5093abee684f175e5fd
anibalcastro <anibalcastro1515@gmail.com>
2022-11-02 22:25:36 -0600

    Login y Registro

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Easy Social Media</title>
+</head>
+
+<body>
+    <section style="
+    background: #0c0909;">
+        <nav style="
+    display: flex;
+    align-items: center;
+    width: 100%;">
+            <div>
+                <a href="/">
+                    <img src="/Images/EASY SOCIAL MEDIA.png" alt="Easy - RS" width="200" height="auto">
+                </a>
+            </div>
+
+            <div style="
+    display: flex;
+    width: 100%;
+    flex-flow: nowrap;
+    justify-content: end;
+    padding: 23px;">
+
+                <button> Prueba 1</button>
+                <button>Prueba 2</button>
+            </div>
+        </nav>
+    </section>
+
+</body>
+
+</html>
