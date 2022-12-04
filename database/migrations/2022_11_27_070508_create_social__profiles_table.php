43e8c05119d320f0e2c95f0ee028899ed17c4f45
anibalcastro <anibalcastro1515@gmail.com>
2022-12-04 15:46:44 -0600

    Correcion

--- database/migrations/2022_11_27_070508_create_social__profiles_table.php
+++ database/migrations/2022_11_27_070508_create_social__profiles_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,7 +18,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('user_profile_id')->unique(); //OAuth_Token
-            $table->longText('user_access_token')->unique(); //OAuth_Secret
+            $table->string('user_access_token')->unique(); //OAuth_Secret
             $table->string('social_network_name');
             $table->timestamps();
         });
